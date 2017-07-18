0c96a74c9a7f71c48990a18c25b0ed60272f57e4
Gil Rosenthal <gil.a.rosenthal@gmail.com>
2017-07-17 21:29:18 -0400

    Add poloniex
    Tested and working, EMC2 however is frozen so I just blocked it.

--- settings.js
+++ settings.js
@@ -143,8 +143,32 @@ let markets = [
             })
         }
 
-    }
+    },
+    {
+        marketName: 'poloniex',
+        URL: 'https://poloniex.com/public?command=returnTicker',
+        toBTCURL: false,
+        last: function (data, coin_prices) { //Where to find the last price of coin in JSON data
+            return new Promise(function (res, rej) {
+                try {
+                    for (var obj in data) {
+                        if(obj.includes('BTC_')&&obj!=="BTC_EMC2") {
+                            let coinName = obj.replace("BTC_", '');
+                            if (!coin_prices[coinName]) coin_prices[coinName] = {};
+                            coin_prices[coinName].poloniex = data[obj].last;
+                        }
+                    }
+                    res(coin_prices);
+                }
+                catch (err) {
+                    console.log(err);
+                    rej(err);
+                }
 
+            })
+        },
+
+    },
 
 ];
 
