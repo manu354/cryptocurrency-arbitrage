6cfc548460e8a271027e03a54339913b126b9b96
Manu Masson <manummasson8@gmail.com>
2017-07-23 12:31:39 +0200

    Fix a bug line 208

--- docs/js/script.js
+++ docs/js/script.js
@@ -130,8 +130,9 @@ $(window).load(function () {
             let coinSource = $("#coin-list-template").html(); //Source
             let coinTemplate = Handlebars.compile(coinSource); // ^ and template for coin and market lists
 
+
             for (let i = data[1].length - 1; i >= 0; i--) { //Loop through coins
-                let context = {market: data[0][i], coin: data[1][i]}; //
+                let context = {market: data[0][0][i], coin: data[1][i]}; //
                 let coin = context.coin, market = context.market;
                 if (data[0][i]) {
                     list.append(marketTemplate(context));
@@ -204,7 +205,7 @@ $(window).load(function () {
                         coin: data[i][0],
                         diff: ((data[i][1] - 1) * 100).toFixed(2),
                         market2price: (data[i][2] * 1000).toPrecision(3),
-                        market2: highestN,
+                        market2: highMarket,
                         market1price: (data[i][3] * 1000).toPrecision(3),
                         market1: lowMarket,
                         pair: {
